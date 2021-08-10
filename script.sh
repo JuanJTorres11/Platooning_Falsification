2c898adc2eef110dfebfe515fbe083c2b82f6595
Juan José Torres <juanjtorres@pm.me>
2021-08-09 22:20:12 -0500

    Automate running different scenarios

---
+++ script.sh
@@ -0,0 +1,10 @@
+#! /bin/bash
+
+python attack_1_bo/falsifier.py --model lgsvl
+python attack_1_ce/falsifier.py --model lgsvl
+python attack_2_bo/falsifier.py --model lgsvl
+python attack_2_ce/falsifier.py --model lgsvl
+python attack_3_bo/falsifier.py --model lgsvl
+python attack_3_ce/falsifier.py --model lgsvl
+python attack_4_bo/falsifier.py --model lgsvl
+python attack_4_ce/falsifier.py --model lgsvl
