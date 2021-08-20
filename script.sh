ffe5d9ea1a3e283a0cb99ba1fd6ba1eeeaa8d081
Juan José Torres <43612178+JuanJTorres11@users.noreply.github.com>
2021-08-20 15:47:18 -0500

    Update script.sh

--- script.sh
+++ script.sh
@@ -2,7 +2,7 @@
 
 python3.8 attack_1_bo/falsifier.py --model lgsvl
 python3.8 attack_1_ce/falsifier.py --model lgsvl
-python3.8 attack_2_bo/falsifier.py --model lgsv
+python3.8 attack_2_bo/falsifier.py --model lgsvl
 python3.8 attack_2_ce/falsifier.py --model lgsvl
 python3.8 attack_3_bo/falsifier.py --model lgsvl
 python3.8 attack_3_ce/falsifier.py --model lgsvl
